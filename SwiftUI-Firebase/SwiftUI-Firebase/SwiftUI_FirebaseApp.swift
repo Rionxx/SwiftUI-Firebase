4b48d408376bb806768d62eb291a8c776ed88e81
Rion <rionfx.42@gmail.com>
2021-08-28 21:55:02 +0900

    Created Project that SwiftUI-Firebase

---
+++ SwiftUI-Firebase/SwiftUI-Firebase/SwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FirebaseApp.swift
+//  SwiftUI-Firebase
+//
+//  Created by FX on 2021/08/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
